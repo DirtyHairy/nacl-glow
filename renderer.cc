44ce2ddc165ace292ed3b81ad349e2707de28785
Christian Speckner <cnspeckn@googlemail.com>
2013-11-11 22:01:03 +0100

    Delay + main loop.

--- renderer.cc
+++ renderer.cc
@@ -1,15 +1,33 @@
 #include "renderer.h"
 
+#include <exception>
+
+#include "sys/time.h"
+#include "unistd.h"
+
 #include "ppapi/cpp/message_loop.h"
 #include "ppapi/cpp/completion_callback.h"
 #include "ppapi/cpp/image_data.h"
 #include "ppapi/cpp/point.h"
 #include "ppapi/cpp/size.h"
+#include "ppapi/cpp/message_loop.h"
 
 namespace {
 
 void dispatch(pp::MessageLoop&, void* user_data) {
     static_cast<glow::Renderer*>(user_data)->_Dispatch();
+}
+
+void delay(timeval& reference, uint32_t delay)
+{
+    timeval current;
+    if (gettimeofday(&current, NULL) != 0) return;
+
+    int32_t actual_delay = delay
+        - (current.tv_sec - reference.tv_sec) * 1000000
+        - current.tv_usec + reference.tv_usec;
+
+    if (actual_delay > 0) usleep(delay);
 }
 
 uint32_t PixelRGB(const uint8_t r, const uint8_t g, const uint8_t b) {
@@ -20,6 +38,8 @@ const uint32_t BLACK = 0xFF000000;
 const uint32_t RED = 0xFF0000FF;
 const uint32_t GREEN = 0xFF00FF00;
 const uint32_t BLUE = 0xFFFF0000;
+
+class EQuit : public std::exception {};
 
 }
 
@@ -76,6 +96,23 @@ void Renderer::_Dispatch() {
     graphics->Flush(pp::CompletionCallback());
 
     logger->Log("Clear OK");
+
+    timeval timestamp;
+    pp::MessageLoop& message_loop = thread->message_loop();
+
+    try {
+        while (true) {
+            if (gettimeofday(&timestamp, NULL) != 0) throw EQuit();
+
+            logger->Log("tick");
+
+            if (message_loop.PostQuit(false) != PP_OK) throw EQuit();
+            if (message_loop.Run() != PP_OK) throw EQuit();
+
+            delay(timestamp, 500000);
+        }
+    }
+    catch(EQuit) {}
 }
 
 }
